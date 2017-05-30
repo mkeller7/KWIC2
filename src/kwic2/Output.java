f4883a902d7584d68bb31abdffbd00c10976f186
Michael Keller <mk@10.16.38.29>
2017-05-30 00:57:31 -0500

    Initial push for Output Filter

---
+++ src/kwic2/Output.java
@@ -0,0 +1,17 @@
+package kwic2;
+
+import java.util.ArrayList;
+
+public class Output implements IFilter{
+
+    @Override
+    public ArrayList<Line> getLine() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setup() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
